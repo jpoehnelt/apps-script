6af6066a120448e5c9619d170fd07ef83df1e415
Justin Poehnelt <jpoehnelt@google.com>
2025-03-28 09:28:06 -0600

    chore: skip pnpm files with prettier

--- .lintstagedrc.commit.js
+++ .lintstagedrc.commit.js
@@ -1,6 +1,8 @@
 export default {
   "**/*.{js,ts,json}": (filenames) =>
-    filenames.length ? [`biome check --write ${filenames.join(" ")}`] : [],
+    filenames.length
+      ? [`biome check --no-errors-on-unmatched --write ${filenames.join(" ")}`]
+      : [],
   "!(**/*.{js,ts,json})": (filenames) =>
     filenames.length ? [`prettier --write ${filenames.join(" ")}`] : [],
 };
